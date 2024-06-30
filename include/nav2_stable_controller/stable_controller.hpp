ab378197e3fff6d2ca0b2ff621e510df080e38d4
Théo <theo.combelles@gmail.com>
2024-06-30 15:20:00 +0900

    controller without last waypoint heading alignment is ready.

---
+++ include/nav2_stable_controller/stable_controller.hpp
@@ -0,0 +1,115 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Author(s): Théo Combelles <theo.combelles@gmail.com>
+ *
+ */
+
+#ifndef NAV2_STABLE_CONTROLLER__STABLE_CONTROLLER_HPP_
+#define NAV2_STABLE_CONTROLLER__STABLE_CONTROLLER_HPP_
+
+#include <cmath>
+
+#include "nav2_core/controller.hpp"
+#include "nav2_core/exceptions.hpp"
+#include "nav2_costmap_2d/footprint_collision_checker.hpp"
+#include "nav2_util/geometry_utils.hpp"
+#include "nav2_util/node_utils.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "pluginlib/class_loader.hpp"
+
+using std::abs;
+using std::hypot;
+using std::max;
+using std::min;
+
+namespace nav2_stable_controller {
+
+struct CmdVelocity {
+    double linear_vel;
+    double angular_vel;
+};
+
+class StableController : public nav2_core::Controller {
+   public:
+    StableController() = default;
+    ~StableController() override = default;
+
+    void configure(const rclcpp_lifecycle::LifecycleNode::WeakPtr& parent, std::string name,
+                   const std::shared_ptr<tf2_ros::Buffer> tf,
+                   const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+    void activate() override;
+    void deactivate() override;
+
+    void cleanup() override;
+    void setPlan(const nav_msgs::msg::Path& path) override;
+
+    void setSpeedLimit(const double& speed_limit, const bool& percentage) override;
+
+    geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped& pose,
+                                                             const geometry_msgs::msg::Twist& velocity,
+                                                             nav2_core::GoalChecker* goal_checker) override;
+
+   protected:
+    nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped& pose);
+
+    bool transformPose(const std::shared_ptr<tf2_ros::Buffer> tf, const std::string frame,
+                       const geometry_msgs::msg::PoseStamped& in_pose,
+                       geometry_msgs::msg::PoseStamped& out_pose,
+                       const rclcpp::Duration& transform_tolerance) const;
+
+    CmdVelocity computeVelocity(const geometry_msgs::msg::Pose& goal_pose);
+
+    geometry_msgs::msg::TwistStamped generateTwistMsg(const geometry_msgs::msg::PoseStamped& pose,
+                                                      const CmdVelocity& cmd);
+
+    void checkCollision(const geometry_msgs::msg::PoseStamped& pose, const CmdVelocity& cmd);
+
+    geometry_msgs::msg::Pose selectGoal(const nav_msgs::msg::Path& transformed_plan);
+
+    /**
+     * @brief Whether collision is imminent
+     * @param robot_pose Pose of robot
+     * @param carrot_pose Pose of carrot
+     * @param linear_vel linear velocity to forward project
+     * @param angular_vel angular velocity to forward project
+     * @param carrot_dist Distance to the carrot for PP
+     * @return Whether collision is imminent
+     */
+    bool isCollisionImminent(const geometry_msgs::msg::PoseStamped&, const double&, const double&,
+                             const double&);
+
+    /**
+     * @brief checks for collision at projected pose
+     * @param x Pose of pose x
+     * @param y Pose of pose y
+     * @param theta orientation of Yaw
+     * @return Whether in collision
+     */
+    bool inCollision(const double& x, const double& y, const double& theta);
+
+    rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+    std::shared_ptr<tf2_ros::Buffer> tf_;
+    std::string plugin_name_;
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+    nav2_costmap_2d::Costmap2D* costmap_;
+    rclcpp::Logger logger_{rclcpp::get_logger("StableController")};
+    rclcpp::Clock::SharedPtr clock_;
+
+    double desired_linear_vel_;
+    double lookahead_dist_;
+    double max_angular_vel_;
+    double max_angular_drift_;
+    rclcpp::Duration transform_tolerance_{0, 0};
+    bool use_collision_detection_;
+    double max_allowed_time_to_collision_;
+
+    nav_msgs::msg::Path global_plan_;
+    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
+    std::unique_ptr<nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D*>>
+        collision_checker_;
+};
+}  // namespace nav2_stable_controller
+
+#endif  // NAV2_STABLE_CONTROLLER__STABLE_CONTROLLER_HPP_
---
+++ src/stable_controller.cpp
@@ -0,0 +1,373 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Author(s): Théo Combelles <theo.combelles@gmail.com>
+
+ */
+
+#include "nav2_stable_controller/stable_controller.hpp"
+
+using nav2_util::declare_parameter_if_not_declared;
+using nav2_util::geometry_utils::euclidean_distance;
+
+namespace nav2_stable_controller {
+
+void StableController::configure(const rclcpp_lifecycle::LifecycleNode::WeakPtr& parent, std::string name,
+                                 const std::shared_ptr<tf2_ros::Buffer> tf,
+                                 const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) {
+    node_ = parent;
+    auto node = node_.lock();
+
+    costmap_ros_ = costmap_ros;
+    costmap_ = costmap_ros_->getCostmap();
+    tf_ = tf;
+    plugin_name_ = name;
+    logger_ = node->get_logger();
+    clock_ = node->get_clock();
+
+    declare_parameter_if_not_declared(node, plugin_name_ + ".desired_linear_vel",
+                                      rclcpp::ParameterValue(0.2));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".max_angular_vel", rclcpp::ParameterValue(1.0));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".transform_tolerance",
+                                      rclcpp::ParameterValue(0.1));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".lookahead_dist", rclcpp::ParameterValue(0.4));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".max_angular_drift", rclcpp::ParameterValue(0.3));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".use_collision_detection",
+                                      rclcpp::ParameterValue(true));
+    declare_parameter_if_not_declared(node, plugin_name_ + ".max_allowed_time_to_collision",
+                                      rclcpp::ParameterValue(1.0));
+
+    double transform_tolerance;
+    node->get_parameter(plugin_name_ + ".transform_tolerance", transform_tolerance);
+    transform_tolerance_ = rclcpp::Duration::from_seconds(transform_tolerance);
+    node->get_parameter(plugin_name_ + ".desired_linear_vel", desired_linear_vel_);
+    node->get_parameter(plugin_name_ + ".max_angular_vel", max_angular_vel_);
+    node->get_parameter(plugin_name_ + ".lookahead_dist", lookahead_dist_);
+    node->get_parameter(plugin_name_ + ".max_angular_drift", max_angular_drift_);
+    node->get_parameter(plugin_name_ + ".use_collision_detection", use_collision_detection_);
+    node->get_parameter(plugin_name_ + ".max_allowed_time_to_collision", max_allowed_time_to_collision_);
+
+    global_pub_ = node->create_publisher<nav_msgs::msg::Path>("received_global_plan", 1);
+
+    // initialize collision checker and set costmap
+    collision_checker_ =
+        std::make_unique<nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D*>>(costmap_);
+    collision_checker_->setCostmap(costmap_);
+}
+
+void StableController::activate() {
+    RCLCPP_INFO(logger_, "Activating controller: %s of type stable_controller::StableController\"  %s",
+                plugin_name_.c_str(), plugin_name_.c_str());
+    global_pub_->on_activate();
+}
+
+void StableController::deactivate() {
+    RCLCPP_INFO(logger_, "Dectivating controller: %s of type stable_controller::StableController\"  %s",
+                plugin_name_.c_str(), plugin_name_.c_str());
+    global_pub_->on_deactivate();
+}
+
+void StableController::cleanup() {
+    RCLCPP_INFO(logger_, "Cleaning up controller: %s of type stable_controller::StableController",
+                plugin_name_.c_str());
+    global_pub_.reset();
+}
+
+void StableController::setPlan(const nav_msgs::msg::Path& path) {
+    global_pub_->publish(path);
+    global_plan_ = path;
+}
+
+void StableController::setSpeedLimit(const double& speed_limit, const bool& percentage) {
+    (void)speed_limit;
+    (void)percentage;
+}
+
+template <typename T>
+int sign(T value) {
+    return (value > 0) - (value < 0);
+}
+
+geometry_msgs::msg::TwistStamped StableController::computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped& pose, const geometry_msgs::msg::Twist& velocity,
+    nav2_core::GoalChecker* goal_checker) {
+    (void)velocity;
+    (void)goal_checker;
+
+    auto goal_pose = selectGoal(transformGlobalPlan(pose));
+
+    auto cmd = computeVelocity(goal_pose);
+
+    checkCollision(pose, cmd);
+
+    return generateTwistMsg(pose, cmd);
+}
+
+geometry_msgs::msg::Pose StableController::selectGoal(const nav_msgs::msg::Path& transformed_plan) {
+    auto goal_pose_it = std::find_if(
+        transformed_plan.poses.begin(), transformed_plan.poses.end(),
+        [&](const auto& ps) { return hypot(ps.pose.position.x, ps.pose.position.y) >= lookahead_dist_; });
+
+    // If the last pose is still within lookahed distance, take the last pose
+    if (goal_pose_it == transformed_plan.poses.end()) {
+        goal_pose_it = std::prev(transformed_plan.poses.end());
+    }
+    return goal_pose_it->pose;
+}
+CmdVelocity StableController::computeVelocity(const geometry_msgs::msg::Pose& goal_pose) {
+    auto cmd = CmdVelocity();
+
+    auto k_p = 1.0;
+    auto k_d = 3.0;
+
+    auto angle_error = std::atan2(goal_pose.position.y, goal_pose.position.x);
+
+    if (goal_pose.position.x > 0) {
+        if (std::abs(angle_error) < max_angular_drift_) {
+            cmd.linear_vel = desired_linear_vel_ * std::cos(angle_error);
+            cmd.angular_vel = -k_p * goal_pose.position.y + k_d * angle_error;
+        } else {
+            cmd.linear_vel = 0.0;
+            cmd.angular_vel = max_angular_vel_ * sign(angle_error);
+        }
+
+    } else {
+        cmd.linear_vel = 0.0;
+        cmd.angular_vel = max_angular_vel_ * sign(angle_error);
+    }
+
+    return cmd;
+}
+
+void StableController::checkCollision(const geometry_msgs::msg::PoseStamped& pose, const CmdVelocity& cmd) {
+    if (use_collision_detection_ && isCollisionImminent(pose, cmd.linear_vel, cmd.angular_vel, 1.0)) {
+        throw nav2_core::PlannerException("StableController detected collision ahead!");
+    }
+}
+
+geometry_msgs::msg::TwistStamped StableController::generateTwistMsg(
+    const geometry_msgs::msg::PoseStamped& pose, const CmdVelocity& cmd) {
+    geometry_msgs::msg::TwistStamped cmd_vel;
+    cmd_vel.header.frame_id = pose.header.frame_id;
+    cmd_vel.header.stamp = clock_->now();
+    cmd_vel.twist.linear.x = cmd.linear_vel;
+    cmd_vel.twist.angular.z = max(-1.0 * abs(max_angular_vel_), min(cmd.angular_vel, abs(max_angular_vel_)));
+    return cmd_vel;
+}
+
+/**
+ * Find element in iterator with the minimum calculated value
+ */
+template <typename Iter, typename Getter>
+Iter min_by(Iter begin, Iter end, Getter getCompareVal) {
+    if (begin == end) {
+        return end;
+    }
+    auto lowest = getCompareVal(*begin);
+    Iter lowest_it = begin;
+    for (Iter it = ++begin; it != end; ++it) {
+        auto comp = getCompareVal(*it);
+        if (comp < lowest) {
+            lowest = comp;
+            lowest_it = it;
+        }
+    }
+    return lowest_it;
+}
+
+nav_msgs::msg::Path StableController::transformGlobalPlan(const geometry_msgs::msg::PoseStamped& pose) {
+    // Original mplementation taken fron nav2_dwb_controller
+
+    if (global_plan_.poses.empty()) {
+        throw nav2_core::PlannerException("Received plan with zero length");
+    }
+
+    // let's get the pose of the robot in the frame of the plan
+    geometry_msgs::msg::PoseStamped robot_pose;
+    if (!transformPose(tf_, global_plan_.header.frame_id, pose, robot_pose, transform_tolerance_)) {
+        throw nav2_core::PlannerException("Unable to transform robot pose into global plan's frame");
+    }
+
+    // We'll discard points on the plan that are outside the local costmap
+    nav2_costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
+    double dist_threshold =
+        std::max(costmap->getSizeInCellsX(), costmap->getSizeInCellsY()) * costmap->getResolution() / 2.0;
+
+    // First find the closest pose on the path to the robot
+    auto transformation_begin = min_by(global_plan_.poses.begin(), global_plan_.poses.end(),
+                                       [&robot_pose](const geometry_msgs::msg::PoseStamped& ps) {
+                                           return euclidean_distance(robot_pose, ps);
+                                       });
+
+    // From the closest point, look for the first point that's further then dist_threshold from the
+    // robot. These points are definitely outside of the costmap so we won't transform them.
+    auto transformation_end =
+        std::find_if(transformation_begin, end(global_plan_.poses), [&](const auto& global_plan_pose) {
+            return euclidean_distance(robot_pose, global_plan_pose) > dist_threshold;
+        });
+
+    // Helper function for the transform below. Transforms a PoseStamped from global frame to local
+    auto transformGlobalPoseToLocal = [&](const auto& global_plan_pose) {
+        // We took a copy of the pose, let's lookup the transform at the current time
+        geometry_msgs::msg::PoseStamped stamped_pose, transformed_pose;
+        stamped_pose.header.frame_id = global_plan_.header.frame_id;
+        stamped_pose.header.stamp = pose.header.stamp;
+        stamped_pose.pose = global_plan_pose.pose;
+        transformPose(tf_, costmap_ros_->getBaseFrameID(), stamped_pose, transformed_pose,
+                      transform_tolerance_);
+        return transformed_pose;
+    };
+
+    // Transform the near part of the global plan into the robot's frame of reference.
+    nav_msgs::msg::Path transformed_plan;
+    std::transform(transformation_begin, transformation_end, std::back_inserter(transformed_plan.poses),
+                   transformGlobalPoseToLocal);
+    transformed_plan.header.frame_id = costmap_ros_->getBaseFrameID();
+    transformed_plan.header.stamp = pose.header.stamp;
+
+    // Remove the portion of the global plan that we've already passed so we don't
+    // process it on the next iteration (this is called path pruning)
+    global_plan_.poses.erase(begin(global_plan_.poses), transformation_begin);
+    global_pub_->publish(transformed_plan);
+
+    if (transformed_plan.poses.empty()) {
+        throw nav2_core::PlannerException("Resulting plan has 0 poses in it.");
+    }
+
+    return transformed_plan;
+}
+
+bool StableController::transformPose(const std::shared_ptr<tf2_ros::Buffer> tf, const std::string frame,
+                                     const geometry_msgs::msg::PoseStamped& in_pose,
+                                     geometry_msgs::msg::PoseStamped& out_pose,
+                                     const rclcpp::Duration& transform_tolerance) const {
+    // Implementation taken as is fron nav_2d_utils in nav2_dwb_controller
+
+    if (in_pose.header.frame_id == frame) {
+        out_pose = in_pose;
+        return true;
+    }
+
+    try {
+        tf->transform(in_pose, out_pose, frame);
+        return true;
+    } catch (tf2::ExtrapolationException& ex) {
+        auto transform = tf->lookupTransform(frame, in_pose.header.frame_id, tf2::TimePointZero);
+        if ((rclcpp::Time(in_pose.header.stamp) - rclcpp::Time(transform.header.stamp)) >
+            transform_tolerance) {
+            RCLCPP_ERROR(rclcpp::get_logger("tf_help"),
+                         "Transform data too old when converting from %s to %s",
+                         in_pose.header.frame_id.c_str(), frame.c_str());
+            RCLCPP_ERROR(rclcpp::get_logger("tf_help"), "Data time: %ds %uns, Transform time: %ds %uns",
+                         in_pose.header.stamp.sec, in_pose.header.stamp.nanosec, transform.header.stamp.sec,
+                         transform.header.stamp.nanosec);
+            return false;
+        } else {
+            tf2::doTransform(in_pose, out_pose, transform);
+            return true;
+        }
+    } catch (tf2::TransformException& ex) {
+        RCLCPP_ERROR(rclcpp::get_logger("tf_help"), "Exception in transformPose: %s", ex.what());
+        return false;
+    }
+    return false;
+}
+
+bool StableController::isCollisionImminent(const geometry_msgs::msg::PoseStamped& robot_pose,
+                                           const double& linear_vel, const double& angular_vel,
+                                           const double& carrot_dist) {
+    // Note(stevemacenski): This may be a bit unusual, but the robot_pose is in
+    // odom frame and the carrot_pose is in robot base frame.
+
+    // check current point is OK
+    if (inCollision(robot_pose.pose.position.x, robot_pose.pose.position.y,
+                    tf2::getYaw(robot_pose.pose.orientation))) {
+        return true;
+    }
+
+    // visualization messages
+    nav_msgs::msg::Path arc_pts_msg;
+    arc_pts_msg.header.frame_id = costmap_ros_->getGlobalFrameID();
+    arc_pts_msg.header.stamp = robot_pose.header.stamp;
+    geometry_msgs::msg::PoseStamped pose_msg;
+    pose_msg.header.frame_id = arc_pts_msg.header.frame_id;
+    pose_msg.header.stamp = arc_pts_msg.header.stamp;
+
+    double projection_time = 0.0;
+    if (fabs(linear_vel) < 0.01 && fabs(angular_vel) > 0.01) {
+        // rotating to heading at goal or toward path
+        // Equation finds the angular distance required for the largest
+        // part of the robot radius to move to another costmap cell:
+        // theta_min = 2.0 * sin ((res/2) / r_max)
+        // via isosceles triangle r_max-r_max-resolution,
+        // dividing by angular_velocity gives us a timestep.
+        double max_radius = costmap_ros_->getLayeredCostmap()->getCircumscribedRadius();
+        projection_time = 2.0 * sin((costmap_->getResolution() / 2) / max_radius) / fabs(angular_vel);
+    } else {
+        // Normal path tracking
+        projection_time = costmap_->getResolution() / fabs(linear_vel);
+    }
+
+    const geometry_msgs::msg::Point& robot_xy = robot_pose.pose.position;
+    geometry_msgs::msg::Pose2D curr_pose;
+    curr_pose.x = robot_pose.pose.position.x;
+    curr_pose.y = robot_pose.pose.position.y;
+    curr_pose.theta = tf2::getYaw(robot_pose.pose.orientation);
+
+    // only forward simulate within time requested
+    int i = 1;
+    while (i * projection_time < max_allowed_time_to_collision_) {
+        i++;
+
+        // apply velocity at curr_pose over distance
+        curr_pose.x += projection_time * (linear_vel * cos(curr_pose.theta));
+        curr_pose.y += projection_time * (linear_vel * sin(curr_pose.theta));
+        curr_pose.theta += projection_time * angular_vel;
+
+        // check if past carrot pose, where no longer a thoughtfully valid command
+        if (hypot(curr_pose.x - robot_xy.x, curr_pose.y - robot_xy.y) > carrot_dist) {
+            break;
+        }
+
+        // store it for visualization
+        pose_msg.pose.position.x = curr_pose.x;
+        pose_msg.pose.position.y = curr_pose.y;
+        pose_msg.pose.position.z = 0.01;
+        arc_pts_msg.poses.push_back(pose_msg);
+
+        // check for collision at the projected pose
+        if (inCollision(curr_pose.x, curr_pose.y, curr_pose.theta)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool StableController::inCollision(const double& x, const double& y, const double& theta) {
+    unsigned int mx, my;
+
+    if (!costmap_->worldToMap(x, y, mx, my)) {
+        RCLCPP_WARN_THROTTLE(
+            logger_, *(clock_), 30000,
+            "The dimensions of the costmap is too small to successfully check for "
+            "collisions as far ahead as requested. Proceed at your own risk, slow the robot, or "
+            "increase your costmap size.");
+        return false;
+    }
+
+    double footprint_cost =
+        collision_checker_->footprintCostAtPose(x, y, theta, costmap_ros_->getRobotFootprint());
+    if (footprint_cost == static_cast<double>(nav2_costmap_2d::NO_INFORMATION) &&
+        costmap_ros_->getLayeredCostmap()->isTrackingUnknown()) {
+        return false;
+    }
+
+    // if occupied or unknown and not to traverse unknown space
+    return footprint_cost >= static_cast<double>(nav2_costmap_2d::LETHAL_OBSTACLE);
+}
+
+}  // namespace nav2_stable_controller
+
+// Register this controller as a nav2_core plugin
+PLUGINLIB_EXPORT_CLASS(nav2_stable_controller::StableController, nav2_core::Controller)
