648d8ae90e7fbf5edb70a004059c89c10c6987dc
Théo <theo.combelles@gmail.com>
2024-06-30 17:02:28 +0900

    added copyright and passed cpplint tests

--- include/nav2_lyapunov_stable_controller/cmd_velocity.hpp
+++ include/nav2_lyapunov_stable_controller/cmd_velocity.hpp
@@ -1,7 +1,25 @@
+// Copyright (c) 2024 LonelyPaprika
+// Author (s) Théo Combelles <theo.combelles@gmail.com>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NAV2_LYAPUNOV_STABLE_CONTROLLER__CMD_VELOCITY_HPP_
+#define NAV2_LYAPUNOV_STABLE_CONTROLLER__CMD_VELOCITY_HPP_
+
 namespace nav2_lyapunov_stable_controller {
 
 struct CmdVelocity {
     double linear_vel;
     double angular_vel;
 };
-}
+}  // namespace nav2_lyapunov_stable_controller
+#endif  // NAV2_LYAPUNOV_STABLE_CONTROLLER__CMD_VELOCITY_HPP_
--- include/nav2_lyapunov_stable_controller/lyapunov_stable_controller.hpp
+++ include/nav2_lyapunov_stable_controller/lyapunov_stable_controller.hpp
@@ -1,14 +1,23 @@
-/*
- * SPDX-License-Identifier: BSD-3-Clause
- *
- *  Author(s): Théo Combelles <theo.combelles@gmail.com>
- *
- */
+// Copyright (c) 2024 LonelyPaprika
+// Author (s) Théo Combelles <theo.combelles@gmail.com>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef NAV2_LYAPUNOV_STABLE_CONTROLLER__LYAPUNOV_STABLE_CONTROLLER_HPP_
 #define NAV2_LYAPUNOV_STABLE_CONTROLLER__LYAPUNOV_STABLE_CONTROLLER_HPP_
 
 #include <cmath>
+#include <memory>
+#include <string>
 
 #include "cmd_velocity.hpp"
 #include "nav2_core/controller.hpp"
